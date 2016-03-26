8197aa116f855edf1862eb123b33dc6eb0b4f58e
niculae.paul <niculae.paul@gmail.com>
2016-03-26 15:56:19 +0200

    Board: Constructor made public Bot: getID methods

--- UTT/UTT/Board.h
+++ UTT/UTT/Board.h
@@ -9,13 +9,13 @@ public:
 	bool CheckIfPlayerCanClose(std::pair<int,int> &PositionToClose, std::vector<int> &s, int board, int player);
 	int PositionToBoard(int value);
 	std::vector<int> empty_positions(std::vector<int> &s, int board);
-
+	Board();
+	~Board();
 private:
 	bool TwoOnRow(std::pair<int, int> &PositionToClose, std::vector<int> &s, int board, int player);
 	bool TwoOnColumn(std::pair<int,int> &PositionToClose, std::vector<int> &s, int board, int player);
 	bool TwoOnFirstDiagonal(std::pair<int, int> &PositionToClose, std::vector<int> &s, int board, int player);
 	bool TwoOnSecondDiagonal(std::pair<int,int> &PositionToClose, std::vector<int> &s, int board, int player);
-	Board();
-	~Board();
+
 };
 
